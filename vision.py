41df8d10ed8b4930202e54f146fd080531c019a4
Mr Tech <koechfestus003@gmail.com>
2024-12-10 13:51:52 +0300

    Vision and text in streamlit

--- vision.py
+++ vision.py
@@ -5,11 +5,31 @@ load_dotenv()
 import streamlit as st
 import os
 import google.generativeai as genai
+from PIL import Image
 
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
 # function to load gemini pro model and get responses
-model=genai.GenerativeModel('gemini-pro')
-def get_gemini_response(question):
-    response=model.generate_content(question)
+model=genai.GenerativeModel('gemini-1.5-flash')
+def get_gemini_response(input, image):
+    if input !="":
+        response=model.generate_content([input, image])
+    else:
+        response=model.generate_content(image)
     return response.text
+# initialize the streamlit app
+st.header("Gemini app using streamlit")
+input =st.text_input("Input prompt: ", key="input")
+
+uploaded_file=st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png", "webp"])
+image=""
+if uploaded_file !=None:
+    image=Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+submit=st.button("Submit query")
+
+# if submit is clicked
+response=get_gemini_response(input, image)
+st.subheader("What the image is about")
+st.write(response) 
