1941566fcaa40adeec5227a335d66c7654a48ad8
Mr Tech <koechfestus003@gmail.com>
2024-12-16 23:03:04 +0300

    Chat with pdf

--- App.py
+++ App.py
@@ -41,5 +41,40 @@ def get_conversational_chain():
     """
     model=ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.6)
 
-    PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    prompt=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain=load_qa_chain(model,chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     
+    new_db =FAISS.load_local("faiss_index", embeddings)
+    docs=new_db.similarity(user_question)
+
+    chain=get_conversational_chain()
+
+    response=chain( {"input_documents": docs, "question":user_question}, return_only_outputs=True )
+    
+    print(response)
+    st.write("Response: ", response["output_text"])
+
+def main():
+    st.set_page_config("Chat with multiple pdf")
+    st.header("Chat with PDF USING Gemini")
+
+    user_question=st.text_input("Ask smart pdf")
+
+    if user_question:
+        user_input(user_question)
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs=st.file_uploader("Upload your PDF Files and Click on the Submit and Process buttons")
+
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text=get_pdf_text(pdf_docs)
+                text_chunks=get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+if __name__ == "__main__":
+    main()
