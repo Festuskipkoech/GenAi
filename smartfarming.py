4305033eb2925e6dc494be5f7226874c8e3c6739
Mr Tech <koechfestus003@gmail.com>
2024-12-18 22:04:55 +0300

    smart farming commit

--- smartfarming.py
+++ smartfarming.py
@@ -234,8 +234,7 @@ def main():
         uploaded_file = st.file_uploader(
             "Upload an image of the plant leaf",
             type=["jpg", "jpeg", "png"]
-        )
-        
+        )        
         if uploaded_file is not None:
             image = Image.open(uploaded_file)
             st.image(image, caption="Uploaded Image", use_column_width=True)
